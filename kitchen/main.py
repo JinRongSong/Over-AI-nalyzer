d0c24046d26fdc33277a3852904b0ab820ea814b
Jin Rong Song <jinrongs@TestAccountsMBP.attlocal.net>
2025-05-20 19:35:59 -0700

    Adding the analyzer files

--- kitchen/main.py
+++ kitchen/main.py
@@ -1,4 +1,9 @@
-def greet(name):
-    print(f"Hello, {name}!")
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.responses import JSONResponse
+from PIL import Image
+import io
+import base64
 
-greet("GitHub")
+
+
